d8c1e6e3e3400799480c24de75f2f32b8f97df85
zhanghui <whnqwe@126.com>
2018-12-05 19:44:35 +0800

    add WebMvcConfig

--- src/main/java/cn/zhangspace/springbootrest/config/WebWvcConfig.java
+++ src/main/java/cn/zhangspace/springbootrest/config/WebWvcConfig.java
@@ -1,4 +1,17 @@
 package cn.zhangspace.springbootrest.config;
 
-public class WebWvcConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebWvcConfig implements WebMvcConfigurer{
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new MappingJackson2HttpMessageConverter());
+    }
 }
